5c127e7669dd564aa380261be216bb61782cdff8
Xinan <youkingan11@gmail.com>
2023-07-24 11:02:20 +0100

    change the profile img

--- Home.py
+++ Home.py
@@ -10,7 +10,7 @@ st.set_page_config(layout="wide")
 col1, col2 = st.columns(2)
 
 with col1:
-    st.image("images/photo.jpeg", width=300)
+    st.image("images/Profile.png", width=300)
 
 with col2:
     st.title("Xinan (Ian) Yang")
